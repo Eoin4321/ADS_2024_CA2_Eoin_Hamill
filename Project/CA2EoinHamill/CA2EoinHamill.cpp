3f779cf10d102c793c38a7c75b6edd0f0155efd0
Eoin4321 <eoinhamill1919@gmail.com>
2024-11-29 22:25:55 +0000

    :construction_worker: Added in preparation for Unit Testing.
    Created a unity testing project.

---
+++ Project/CA2EoinHamill/CA2EoinHamill.cpp
@@ -0,0 +1,13 @@
+// 
+//
+
+#include <iostream>
+#include "BSTNode.h"
+#include "BinaryTree.h"
+
+int main()
+{
+    
+}
+
+
---
+++ Project/MethodsTest/MethodsTest.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CA2EoinHamill/BinaryTree.h"
+#include "../CA2EoinHamill/BSTNode.h"
+
+//Everything in this namespace includes all the stuff I need for Unit Testing
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MethodsTest
+{
+	TEST_CLASS(MethodsTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
